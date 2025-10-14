e94c7ffaae5946d23bd56476fb757256f8f1c416
pedrokrukz <ppok306@gmail.com>
2025-10-14 12:05:43 -0300

    chore: ajustar mensagem para indicar uso do SQLite

--- src/main/java/remote/ServidorMain.java
+++ src/main/java/remote/ServidorMain.java
@@ -1,5 +1,4 @@
 package remote;
-
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
@@ -8,6 +7,6 @@ public class ServidorMain {
         LocateRegistry.createRegistry(1099);
         EstoqueServico servico = new EstoqueServiceImpl();
         Naming.rebind("rmi://localhost:1099/EstoqueService", servico);
-        System.out.println("Servidor RMI iniciado e serviço EstoqueService registrado...");
-    }
+        System.out.println("Servidor de estoque rodando com SQLite...");
+    }
 }
