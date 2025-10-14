dd42704f818811438be329db1811b5e6a2055b20
pedrokrukz <ppok306@gmail.com>
2025-10-14 12:01:40 -0300

    feat: criar classe ServidorMain no package remote

--- src/main/java/remote/ServidorMain.java
+++ src/main/java/remote/ServidorMain.java
@@ -1,10 +1,11 @@
-
 package remote;
 
-/**
- *
- * @author ppok3
- */
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
 public class ServidorMain {
-    
+    public static void main(String[] args) throws Exception {
+        LocateRegistry.createRegistry(1099);
+        System.out.println("Servidor RMI iniciado na porta 1099...");
+    }
 }
