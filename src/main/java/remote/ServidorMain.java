f44d981055b494fc76d1c6d7401d6aab09588347
pedrokrukz <ppok306@gmail.com>
2025-10-14 12:02:36 -0300

    feat: adicionar conexão do serviço EstoqueServiceImpl ao servidor

--- src/main/java/remote/ServidorMain.java
+++ src/main/java/remote/ServidorMain.java
@@ -6,6 +6,8 @@ import java.rmi.registry.LocateRegistry;
 public class ServidorMain {
     public static void main(String[] args) throws Exception {
         LocateRegistry.createRegistry(1099);
-        System.out.println("Servidor RMI iniciado na porta 1099...");
+        EstoqueServico servico = new EstoqueServiceImpl();
+        Naming.rebind("rmi://localhost:1099/EstoqueService", servico);
+        System.out.println("Servidor RMI iniciado e serviço EstoqueService registrado...");
     }
 }
