6fe8120ef3b9236ee249a34c52bae0ee1c41302e
IanAkira <ian.fujimori09@gmail.com>
2025-10-14 14:39:31 -0300

    feat: Conexão com banco SQLite e Metodo "inicializarTabelas"

--- src/main/java/remote/EstoqueServiceImpl.java
+++ src/main/java/remote/EstoqueServiceImpl.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package remote;
 
-/**
- *
- * @author ianfu
- */
-public class EstoqueServiceImpl {
-    
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.sql.*;
+import java.util.*;
+import model.Movimentacao;
+import model.Produto;
+
+public class EstoqueServiceImpl extends UnicastRemoteObject implements EstoqueServico {
+
+    private final Connection conn;
+
+    public EstoqueServiceImpl() throws RemoteException {
+        super();
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:estoque.db");
+            inicializarTabelas();
+        } catch (SQLException e) {
+            throw new RemoteException("Erro ao conectar ao banco", e);
+        }
+    }
+
+    private void inicializarTabelas() throws SQLException {
+        try (Statement st = conn.createStatement()) {
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS produto ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "nome TEXT,"
+                    + "preco REAL,"
+                    + "unidade TEXT,"
+                    + "quantidade INTEGER,"
+                    + "qtdMin INTEGER,"
+                    + "qtdMax INTEGER,"
+                    + "categoria TEXT)");
+        }
+
+    }
+
 }
