165550418ecb49aa19ad62f79a4a6a79d6f6f9ed
pedrokrukz <ppok306@gmail.com>
2025-10-28 17:45:56 -0300

    feat: criação da TelaProdutosCriticos: estrutura base e janela

---
+++ src/main/java/frontend/TelaProdutosCriticos.java
@@ -0,0 +1,28 @@
+package frontend;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class TelaProdutosCriticos extends JFrame {
+
+    private JTable tabela;
+    private DefaultTableModel modeloTabela;
+
+    public TelaProdutosCriticos() {
+        setTitle("⚠ Produtos Abaixo do Mínimo");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+
+        modeloTabela = new DefaultTableModel(new String[]{
+                "Produto", "Quantidade Mínima", "Quantidade em Estoque"
+        }, 0) {
+            @Override public boolean isCellEditable(int row, int column) { return false; }
+        };
+
+        tabela = new JTable(modeloTabela);
+        tabela.setFillsViewportHeight(true);
+
+        add(new JScrollPane(tabela), BorderLayout.CENTER);
+    }
+}
